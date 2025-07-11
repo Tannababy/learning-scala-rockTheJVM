5b97c452cfdd9af4a55eb930bf2dbee50e42b745
Lotanna <tannaloamobi@gmail.com>
2025-07-11 19:56:11 +0100

    learning about data types

---
+++ src/main/scala/lectures/ValuesVariablesTypes.scala
@@ -0,0 +1,26 @@
+package lectures
+
+// extends App to run in terminal
+object ValuesVariablesTypes extends App{
+
+  // Vals are immutable
+  val x: Int = 45
+  println(x)
+
+  // Var are mutable and useful for side effects
+  var y: Int = 1
+  y += 1
+  println(y)
+
+  // compiler can automatically infer types when omitted
+  // basic types: Boolean, Int, Long (must end with L), Double,
+  //              Float (must end wit f)
+  // Characters & Strings
+
+  var aBoolean = false
+  var aLong = 123445566777L
+  var aDouble = 3.456
+  var aFloat = 24.5f
+  var aChar = 'L'
+  var aString = "This is going well"
+}
